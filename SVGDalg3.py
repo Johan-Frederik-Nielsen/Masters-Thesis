52bcd6aedbf1cfe4cf484129f373e2ece2edc979
Johan-Frederik Nielsen <62258452+Johan-Frederik-Nielsen@users.noreply.github.com>
2022-04-09 07:45:42 +0200

    Add files via upload

--- SVGDalg3.py
+++ SVGDalg3.py
@@ -4,40 +4,50 @@ import matplotlib.pyplot as plt
 import time
 import seaborn as sns
 
-n=111
+n=31
 x=torch.empty((2,n*n))
+interval_size=3.5
 for i in range(n):
     for j in range(n):
-        x[0,i*n+j]=(i-np.floor(n/2))/n*2*5
-        x[1,i*n+j]=(j-np.floor(n/2))/n*2*5
+        x[0,i*n+j]=(i-np.floor(n/2))/n*2*interval_size
+        x[1,i*n+j]=(j-np.floor(n/2))/n*2*interval_size
+
+n=n**2
 
 m=torch.distributions.multivariate_normal.MultivariateNormal(torch.zeros(2), torch.eye(2))
-#print(m.log_prob(torch.zeros(2)))
-h=10.0
+
 epsilon=1
-alpha=1   
+alpha=0.2 
 
 for l in range(25):
     t=time.time()
-    x2=(torch.matmul(torch.ones(2,n**2,1),x[:,None]))
-    x3=(x2-torch.transpose(x2,1,2))
-    #x3[:,i,j]=xj-xi
-    x4=torch.exp(-1.0/h*torch.sum(torch.pow(x3,2),0))[None,:,:]
-    #x4[i,j]=kernel(xj,xi)
-    x5=x4*(-1.0/h*2.0*x3)
-    #x5[i,j]=k_grad(xj,xi)
-    #x6=-x2
-    x6=torch.exp(torch.sum((-0.5*torch.pow(x2,2)),0))/(2*np.pi)
-    x6=torch.mul(-x2,torch.stack([x6,x6],dim=0))
-    #x6[:,i,j]=logp_grad(xj)
-    #x7=1/n**2*x4*x6+1/n**2*x5
-    #x7=1/n**2*(x6*x4+(torch.exp(torch.sum((-0.5*torch.pow(x2,2)),0))/(2*np.pi))*x5)*torch.pow(torch.exp(torch.sum((-0.5*torch.pow(x2,2)),0))/(2*np.pi),alpha-1)/(1/25**alpha)
-    x7=1/n**2*(x6*x4+(torch.exp(torch.sum((-0.5*torch.pow(x2,2)),0))/(2*np.pi))*x5)*torch.pow(25*torch.exp(torch.sum((-0.5*torch.pow(x2,2)),0))/(2*np.pi),alpha-1)/(1/25)
-    #x7[:,i,j]=Phi(xi,xj)
-    x8=torch.sum(x7,2)
-    #x8[:,i]=Phi(xi)
-    x=x+epsilon*x8
+    #x_ntimes[:,i,j]=x_i
+    x_ntimes=(torch.matmul(torch.ones(2,n,1),x[:,None]))
+    #x_difference[:,i,j]=x_i-x_j
+    x_difference=(x_ntimes-torch.transpose(x_ntimes,1,2))
+    #h=median(||x_i-x_j||_2)^2/log(n)
+    h=float(torch.pow(torch.median(torch.sort(torch.flatten(torch.triu(torch.pow(torch.sum(torch.pow(x_difference,2),dim=0),0.5))))[0][int((n**2+n)/2):]),2)/np.log(n))
+    #k[i,j]=k(x_i,x_j)
+    k=torch.exp(-1.0/h*torch.sum(torch.pow(x_difference,2),0))[None,:,:]
+    #grad_k=\nabla_x k   
+    grad_k=k*(-1.0/h*2.0*x_difference)
+    #grad_p=\nabla_x p
+    grad_p=torch.exp(torch.sum((-0.5*torch.pow(x_ntimes,2)),0))/(2*np.pi)
+    grad_p=torch.mul(-x_ntimes,torch.stack([grad_p,grad_p],dim=0))
+    #p[i,j]=p(x_i,x_j)
+    p=torch.exp(torch.sum((-0.5*torch.pow(x_ntimes,2)),0))/(2*np.pi)
+    #phi_summand[:,i,j]=j'th summand of update
+    print("MINIMUM",torch.min(p))
+    phi_summand=1/n*(grad_p*k+p*grad_k)*torch.pow(25*p,alpha-1)/(1/25)
+    #phi
+    phi=torch.sum(phi_summand,2)
+    x=x+epsilon*phi
     print(l,time.time()-t)
+    #print("h =",h)
+    #print("||grad_p|| =",float(torch.norm(grad_p)))
+    print("||grad_p*k|| =",float(torch.norm(grad_p*k)))
+    print("||p*grad_k|| =",float(torch.norm(p*grad_k)))
+    print("IMPORTANT NUMBER =", float(torch.norm(grad_p*k))/float(torch.norm(p*grad_k)))
     
 x1=x[0,:]
 y1=x[1,:]
