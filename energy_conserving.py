2d358e7e93eda1e85f36f3c8304f59516a46f489
OlaRonning <ronning@pm.me>
2022-05-23 16:36:30 +0200

    debugging

--- energy_conserving.py
+++ energy_conserving.py
@@ -7,6 +7,7 @@ from pyro.distributions import Normal, MultivariateNormal
 torch.manual_seed(0)
 
 verbose = False
+debug = False
 
 
 def load_data():
@@ -25,39 +26,30 @@ def load_data():
     return torch.concat(xs, 0), torch.concat(ys, 0)
 
 
-def kernel_value_grad(particles):
-    a_sqr = (particles ** 2).sum(1)
-    diff = torch.matmul(particles, particles.T)
-    sq_dist = a_sqr[None, :] + a_sqr[:, None] - 2 * diff
-    h = torch.median(sq_dist) / np.log(n_particles)
-    value = torch.exp(-1.0 / h * sq_dist)
-
-    grad = -2.0 / h * (particles[None, :] - particles[:, None]) * value[..., None]
-    return value, grad
-
-
 def old_step(theta, state_sum):
     # theta_ntimes[i,j,:]=theta[j,:]
     theta_ntimes = theta[None, :, :].repeat(n_particles, 1, 1)
     # theta_difference[i,j,:]=theta[j,:]-theta[i,:]
     theta_difference = (theta_ntimes - torch.transpose(theta_ntimes, 0, 1))
     # h
-    h = float(torch.median(torch.sort(
-        torch.flatten(torch.flatten(torch.triu(torch.pow(torch.sum(torch.pow(theta_difference, 2), 2), 0.5)))))[0][
-                           int((n_particles ** 2 + n_particles) / 2):])) ** 2 / np.log(n_particles)
+    sq_dist = torch.linalg.norm(theta[None, :] - theta[:, None], dim=-1)
+    h = torch.median(sq_dist) ** 2 / np.log(n_particles)
+    # h = float(torch.median(torch.sort(
+    #     torch.flatten(torch.flatten(torch.triu(torch.pow(torch.sum(torch.pow(theta_difference, 2), 2), 0.5)))))[0][
+    #                        int((n_particles ** 2 + n_particles) / 2):])) ** 2 / np.log(n_particles)
     # k[i,j]=k(x_j,x_i)
     k = torch.exp(-1.0 / h * torch.sum(torch.pow(theta_difference, 2), 2))
     # grad_k[i,j,:]=\nabla_x_j k(x_j,x_i)
     grad_k = (-1.0 / h * 2.0 * theta_difference * k[:, :, None])
     # grad_p1=-theta
-    grad_p1 = (data[:, :, None].repeat(1, 1, 50) * torch.transpose(
+    grad_p1 = (data[:, :, None].repeat(1, 1, 5) * torch.transpose(
         (torch.exp(-torch.matmul(theta, torch.transpose(data, 0, 1))))[:, None, :], 0, 2).repeat(1, 28, 1)) / (
                   torch.transpose((1 + torch.exp(-torch.matmul(theta, torch.transpose(data, 0, 1))))[:, None, :], 0,
                                   2).repeat(1, 28, 1))
-    grad_p0 = -(data[:, :, None].repeat(1, 1, 50) * torch.transpose(
-        (torch.exp(torch.matmul(theta, torch.transpose(data, 0, 1))))[:, None, :], 0, 2).repeat(1, 28, 1)) / (
+    grad_p0 = -(data[:, :, None] * torch.transpose(
+        (torch.exp(torch.matmul(theta, torch.transpose(data, 0, 1))))[:, None, :], 0, 2)) / (
                   torch.transpose((1 + torch.exp(torch.matmul(theta, torch.transpose(data, 0, 1))))[:, None, :], 0,
-                                  2).repeat(1, 28, 1))
+                                  2))
     # print("Here: ",torch.Tensor.size(data[:,:,None].repeat(1,1,50)),torch.Tensor.size(torch.transpose((1+torch.exp(torch.matmul(theta,torch.transpose(data,0,1))))[:,None,:],0,2).repeat(1,28,1)))
     grad_p = grad_p0 * (1 - obs[:, None, None]).repeat(1, 28, n_particles) + grad_p1 * obs[:, None, None].repeat(1, 28,
                                                                                                                  n_particles)
@@ -69,8 +61,10 @@ def old_step(theta, state_sum):
     phi = torch.sum(phi_summand, 1)
     # print(torch.Tensor.size(phi),torch.Tensor.size(theta))
     state_sum = state_sum + torch.pow(phi, 2)
+
     epsilon = 0.00001 * phi * torch.pow(state_sum, 0.5)
-    test_tensor = (data[:, :, None].repeat(1, 1, 50) * torch.transpose(
+
+    test_tensor = (data[:, :, None].repeat(1, 1, 5) * torch.transpose(
         (torch.exp(torch.matmul(theta, torch.transpose(data, 0, 1))))[:, None, :], 0, 2).repeat(1, 28, 1))
     if torch.max(test_tensor != test_tensor) == 1:
         print("Error in iteration", l)
@@ -78,46 +72,70 @@ def old_step(theta, state_sum):
             torch.transpose((torch.exp(torch.matmul(theta, torch.transpose(data, 0, 1))))[:, None, :], 0, 2).repeat(1,
                                                                                                                     28,
                                                                                                                     1)),
-            torch.Tensor.size(data[:, :, None].repeat(1, 1, 50)), torch.Tensor.size(-(
-                    data[:, :, None].repeat(1, 1, 50) * torch.transpose(
+            torch.Tensor.size(data[:, :, None].repeat(1, 1, 5)), torch.Tensor.size(-(
+                    data[:, :, None].repeat(1, 1, 5) * torch.transpose(
                 (torch.exp(torch.matmul(theta, torch.transpose(data, 0, 1))))[:, None, :], 0, 2).repeat(1, 28, 1))))
         # print(torch.exp(torch.matmul(theta,torch.transpose(data,0,1))))
         print(torch.max(torch.matmul(theta, torch.transpose(data, 0, 1))))
         # print(test_tensor)
-    theta += epsilon
+    theta = theta + epsilon
     return theta, state_sum
 
 
+def kernel_value_grad(particles):
+    sq_dist = torch.linalg.norm(particles[None, :] - particles[:, None], dim=-1)
+    h = torch.median(sq_dist) ** 2 / np.log(n_particles)
+    value = torch.exp(-1.0 / h * sq_dist ** 2)
+
+    grad = -2.0 / h * (particles[None, :] - particles[:, None]) * value[..., None]
+    return h, value, grad
+
+
 def step(particles, state_sum):
-    k, grad_k = kernel_value_grad(particles)
+    h, k, grad_k = kernel_value_grad(particles)
+    if debug:
+        theta_ntimes = particles[None, :, :].repeat(n_particles, 1, 1)
+        # theta_difference[i,j,:]=theta[j,:]-theta[i,:]
+        theta_difference = (theta_ntimes - torch.transpose(theta_ntimes, 0, 1))
+        # h
+
+        h_old = float(torch.median(
+            torch.sort(
+                torch.flatten(torch.flatten(torch.triu(torch.pow(torch.sum(torch.pow(theta_difference, 2), 2), 0.5)))))[
+                0][
+            int((n_particles ** 2 + n_particles) / 2):])) ** 2 / np.log(n_particles)
+        # k[i,j]=k(x_j,x_i)
+        k_old = torch.exp(-1.0 / h * torch.sum(torch.pow(theta_difference, 2), 2))
+        grad_k_old = (-1.0 / h * 2.0 * theta_difference * k[:, :, None])
+
+        assert torch.allclose(k_old, k)
+        assert torch.allclose(grad_k_old, grad_k)
 
     # grad_p1=-theta
-    grad_p0 = (1 / (1 + torch.exp(-torch.matmul(data, particles.T))))
+    grad_p1 = (1 / (1 + torch.exp(torch.matmul(data, particles.T))))
+    grad_p0 = -(1 / (1 + torch.exp(-torch.matmul(data, particles.T))))
 
-    # grad_p0 + grad_p1 = 1 => obs * (grad_p0 + grad_p1) = obs
-    grad_p = data[:, None, :] * (grad_p0 - obs[:, None])[..., None]
-    grad_p = torch.sum(grad_p, 0) - particles
+    grad_p = data[..., None] * (grad_p0 - (grad_p0 + grad_p1) * obs[:, None])[:, None, :]
+    grad_p = torch.sum(grad_p, 0).T - particles
 
-    phi_summand = (1 / n_particles * (grad_p[:, None, :] * k[:, :, None] + grad_k))
-    phi = torch.sum(phi_summand, 1)
+    phi = (1 / n_particles * (grad_p[:, None, :] * k[:, :, None] + grad_k)).sum(1)
 
-    # print(torch.Tensor.size(phi),torch.Tensor.size(theta))
-    state_sum = state_sum + torch.pow(phi, 2)
-    epsilon = 0.00001 * phi * torch.pow(state_sum, 0.5)
+    state_sum = state_sum + phi ** 2
+    epsilon = 0.00001 * phi * state_sum.sqrt()
 
     if torch.any(torch.isnan(torch.exp(torch.matmul(particles, torch.transpose(data, 0, 1))))):
         raise ValueError('NaN in particles.')
-    particles += epsilon
+    particles = particles + epsilon
     return particles, state_sum
 
 
 def kl(particles, data, obs, dist_q):
     p_current = torch.pow(
         1 + torch.exp(-torch.matmul(particles, torch.transpose(data, 0, 1))),
-        -(obs[None, :].repeat(50, 1)),
+        -(obs[None, :].repeat(5, 1)),
     ) * torch.pow(
         1 + torch.exp(torch.matmul(particles, torch.transpose(data, 0, 1))),
-        (obs[None, :].repeat(50, 1)) - 1,
+        (obs[None, :].repeat(5, 1)) - 1,
     )
 
     p_current = torch.prod(p_current, 1) * torch.exp(dist_q.log_prob(particles))
@@ -131,7 +149,7 @@ def kl(particles, data, obs, dist_q):
 
 
 if __name__ == "__main__":
-    n_particles = 50
+    n_particles = 5
 
     data, obs = load_data()
     n_data, n_feats = data.shape
@@ -150,9 +168,11 @@ if __name__ == "__main__":
     n_iterations = 100
     kl_init = kl(particles, data, obs, dist_q)
     for l in tqdm.tqdm(range(n_iterations)):
-        old_particles, old_state_sum = old_step(particles, state_sum)
+        if debug:
+            old_particles, old_state_sum = old_step(particles, state_sum)
         particles, state_sum = step(particles, state_sum)
-        assert torch.allclose(old_particles, particles)  # for testing
+        if debug:
+            assert torch.allclose(state_sum, old_state_sum)
 
     kl_final = kl(particles, data, obs, dist_q)
     print(kl_final)
